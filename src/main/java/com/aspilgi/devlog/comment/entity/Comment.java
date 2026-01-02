ac529dc51a30ad68f2b929cd56547adababba5c1
ASPILGI <elysion715@gmail.com>
2026-01-02 21:32:01 +0900

    feat: comment entity 추가

---
+++ src/main/java/com/aspilgi/devlog/comment/entity/Comment.java
@@ -0,0 +1,49 @@
+package com.aspilgi.devlog.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt =  LocalDateTime.now();
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
